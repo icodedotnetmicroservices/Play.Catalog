11f4e66dace50e37f061c05b60b913957eeaa44c
docteurjay <aristejean@gmail.com>
2022-04-01 07:26:04 -0400

    Securing the Catalog microservice

--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -1,5 +1,6 @@
 using MassTransit;
 using MassTransit.Definition;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -35,10 +36,18 @@ namespace Play.Catalog.Service
                      .AddMongoRepository<Item>("items")
                      .AddMassTransitWithRabbitMQ();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.Authority = "https://localhost:5003";
+                        options.Audience = serviceSettings.ServiceName;
+                    });
+
             services.AddControllers(options =>
             {
                 options.SuppressAsyncSuffixInActionNames = false;
             });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Catalog.Service", Version = "v1" });
@@ -54,17 +63,19 @@ namespace Play.Catalog.Service
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Catalog.Service v1"));
 
-               app.UseCors(builder => 
-               {
-                   builder.WithOrigins(Configuration[AllowedOriginSetting])
-                          .AllowAnyHeader()
-                          .AllowAnyMethod();
-               });
+                app.UseCors(builder =>
+                {
+                    builder.WithOrigins(Configuration[AllowedOriginSetting])
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
